d87eb3b7aa91fa843f4b3c352fee227e6bc4042b
Balaji H P <balaji.hp@position2.com>
2024-05-02 16:02:49 +0530

    updated script

--- demo/simpleValidation.js
+++ demo/simpleValidation.js
@@ -1,177 +1,188 @@
+/**
+ * jQuery Simple Form Validation
+ * Copyright (c) 2017 Position2 Inc.
+ * Licensed under MIT (http://opensource.org/licenses/MIT)
+ * https://github.com/Position2/jQuery-Simple-Form-Validation
+ */
 (function ($) {
 	$.fn.simpleValidation = function (options, callback) {
-		const defaults = {
-			errorFieldClass: "error",
-			errorMsgTag: "span",
-			errorMsgClass: "errormsg",
-			errorMsg: "Required Field",
-			otherErrorMsg: {
-				email: "Please enter a valid email",
-				companyemail: "Please enter a company email",
-				alphabet: "Please enter letters only",
-				alphabetwithspace: "Please enter letters and spaces only",
-				number: "Please enter numbers only",
-				numberwithspace: "Please enter numbers and spaces only",
-				alphanumeric: "Please don't enter any special characters or spaces",
-				alphanumericwithspace: "Please don't enter any special characters",
-				compare: "Please enter the same value again",
-				minlength: "Please enter a minimum of {n} letters",
+		var settings = $.extend(
+			{
+				errorFieldClass: "error",
+				errorMsgTag: "span",
+				errorMsgClass: "errormsg",
+				errorMsg: "Required Field",
+				otherErrorMsg: {
+					email: "Please enter a valid email",
+					companyemail: "Please enter a company email",
+					alphabet: "Please enter letters only",
+					alphabetwithspace: "Please enter letters and spaces only",
+					number: "Please enter numbers only",
+					numberwithspace: "Please enter numbers and spaces only",
+					alphanumeric: "Please don't enter any special characters or spaces",
+					alphanumericwithspace: "Please don't enter any special characters",
+					compare: "Please enter the same value again",
+					minlength: "Please enter a minimum of {n} characters",
+				},
+				beforeSubmit: function () {},
 			},
-			beforeSubmit: "",
-		};
+			options,
+		);
 
-		const settings = $.extend({}, defaults, options);
-
-		const removeErrorMsg = (elem) => {
-			const $elem = $(elem);
-			const $errorElem = $elem.next("." + settings.errorMsgClass);
-			$elem.removeClass(settings.errorFieldClass);
-			if ($errorElem.length > 0) $errorElem.remove();
-		};
-
-		const addErrorMsg = (elem, msg, errorClassno) => {
-			const $elem = $(elem);
-			if (!errorClassno) $elem.addClass(settings.errorFieldClass);
-			if ($elem.next("." + settings.errorMsgClass).length <= 0) {
-				const $errorElem = $("<" + settings.errorMsgTag + "/>", {
-					class: settings.errorMsgClass,
-				}).text(msg);
-				$errorElem.insertAfter($elem);
-			}
-		};
-
-		const validate = (elem) => {
-			const $elem = $(elem);
-			const val = $elem.val().trim();
-			const name = $elem.attr("name");
-			const required = $elem.attr("data-sfv-required");
-			const minLength = $elem.attr("data-sfv-minlength") || 0;
-			const pattern = $elem.attr("data-sfv-regex");
-			const compare = $elem.attr("data-sfv-compare");
-			const compareElem = $(compare);
-			const compareVal =
-				compare !== "" && typeof compare !== "undefined"
-					? compareElem.val().trim()
-					: "";
-			const patternErrorMsg = $elem.attr("data-sfv-regEx-errorMsg");
-			const requiredErrorMsg = $elem.attr("data-sfv-require-errorMsg");
-
-			if (val !== "") {
-				switch ($elem.attr("type")) {
-					case "email":
-						!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(val)
-							? addErrorMsg(
-									$elem,
-									patternErrorMsg || settings.otherErrorMsg.email,
-							  )
-							: removeErrorMsg($elem);
-						break;
-					case "companyemail":
-						// Update the regular expression for company email validation
-						!/^[\w+\-.]+@(?!gmail\.com|hotmail\.com|live\.com|outlook\.com|yahoo\.com|ymail\.com|rocketmail\.com|aol\.com|mac\.com|me\.com|icloud\.com|inbox\.com|sina\.com|qq\.com|foxmail\.com|163\.com|126\.com|189\.cn|263\.net|yeah\.net|gmx\.com|gmx\.net|mail\.com|mail\.ru|rambler\.ru|lenta\.ru|autorambler\.ru|myrambler\.ru|ro\.ru|yandex\.ru|zoho\.com|msn\.com|webtown\.com|rediffmail\.com)([\w\-.]+@[^\s@]+\.[^\s@]+)?$/.test(
-							val,
-						)
-							? addErrorMsg(
-									$elem,
-									patternErrorMsg || settings.otherErrorMsg.companyemail,
-							  )
-							: removeErrorMsg($elem);
-						break;
-					// Add other cases for validation types as needed
-					default:
-						// Handle other validation types
-						break;
-				}
-			} else if (required === "yes") {
-				addErrorMsg($elem, requiredErrorMsg || settings.errorMsg);
-			}
-		};
-
-
-		const validateChRb = (elem) => {
-			const $elem = $(elem);
-			const name = $elem.attr("name");
-			const requiredErrorMsg = $elem.attr("data-sfv-require-errorMsg");
-			const $siblingElems = $("[name='" + name + "']", this);
-			const $lastSiblingElem = $siblingElems.last()[0].nextSibling;
-			!$siblingElems.is(":checked")
-				? addErrorMsg(
-						$lastSiblingElem,
-						requiredErrorMsg || settings.errorMsg,
-						true,
-				  )
-				: removeErrorMsg($lastSiblingElem);
-		};
+		// Remove error message on click
+		$("body").on(
+			"click",
+			settings.errorMsgTag + "." + settings.errorMsgClass,
+			function () {
+				$(this).fadeOut(function () {
+					$(this).remove();
+				});
+			},
+		);
 
 		return this.each(function () {
-			const $form = $(this);
-			const $formAjax = $form.attr("data-sfv-ajax") || false;
-			const valElems = $(
-				"input[data-sfv-required='yes'],input[data-sfv-validation],input[data-sfv-regex],input[data-sfv-compare],input[data-sfv-minlength],select[data-sfv-required],textarea[data-sfv-required]",
-				$form,
-			);
-			const valChRaElems = $(
-				"input[data-sfv-required='yes'][type='checkbox'],input[data-sfv-required='yes'][type='radio']",
-				$form,
-			);
+			var form = $(this),
+				ajax = form.attr("data-sfv-ajax") || false,
+				minLen = form.attr("data-sfv-minlength") || 0,
+				requiredElems = form.find("[data-sfv-required='yes']"),
+				compareElem = $("[data-sfv-compare]", form);
 
 			// Disable HTML5 default validation
-			$form.attr("novalidate", "");
+			form.attr("novalidate", "");
 
-			// On Form submit
-			$form.on("submit", function (e) {
-				valElems.add(valChRaElems).each(function () {
-					const type = $(this).attr("type");
+			// Add error message
+			function addErrorMessage(element, message, isErrorClass) {
+				var target = $(element);
+				if (!isErrorClass) target.addClass(settings.errorFieldClass);
+				if (target.next("." + settings.errorMsgClass).length <= 0)
+					$("<" + settings.errorMsgTag + "/>", {
+						class: settings.errorMsgClass,
+						text: message,
+					}).insertAfter(target);
+			}
+
+			// Remove error message
+			function removeErrorMessage(element) {
+				var target = $(element),
+					nextErrorMessage = target.next("." + settings.errorMsgClass);
+				target.removeClass(settings.errorFieldClass);
+				if (nextErrorMessage.length > 0) nextErrorMessage.remove();
+			}
+
+			// Validate input
+			function validateInput(input) {
+				var element = $(input),
+					value = element.val().trim(),
+					required = element.attr("data-sfv-required"),
+					minLength = element.attr("data-sfv-minlength") || 0,
+					pattern = element.attr("data-sfv-regex"),
+					compare = element.attr("data-sfv-compare"),
+					compareElement = $(compare),
+					compareValue = compare ? compareElement.val().trim() : "",
+					patternErrorMessage = element.attr("data-sfv-regEx-errorMsg"),
+					requiredErrorMessage = element.attr("data-sfv-require-errorMsg");
+
+				if (value !== "") {
+					if (pattern) {
+						pattern = new RegExp("^" + pattern + "$");
+						!pattern.test(value)
+							? addErrorMessage(
+									element,
+									patternErrorMessage || settings.errorMsg,
+							  )
+							: removeErrorMessage(element);
+					} else if (compare && value !== compareValue) {
+						addErrorMessage(compareElement, settings.otherErrorMsg.compare);
+					} else if (minLength > 0 && value.length < minLength) {
+						addErrorMessage(
+							element,
+							settings.otherErrorMsg.minlength.replace("{n}", minLength),
+						);
+					} else {
+						removeErrorMessage(element);
+					}
+				} else if (required === "yes") {
+					addErrorMessage(element, requiredErrorMessage || settings.errorMsg);
+				}
+			}
+
+			// Validate checkbox and radio button
+			function validateCheckboxRadio(element) {
+				var group = $(element).attr("name"),
+					errorMessage = $(element).attr("data-sfv-require-errorMsg");
+				if ($("[name='" + group + "']:checked").length === 0) {
+					addErrorMessage(
+						$("[name='" + group + "']")
+							.last()
+							.next()[0],
+						errorMessage || settings.errorMsg,
+						true,
+					);
+				} else {
+					removeErrorMessage(
+						$("[name='" + group + "']")
+							.last()
+							.next()[0],
+					);
+				}
+			}
+
+			// On form submit
+			form.on("submit", function (e) {
+				var formData = $(this);
+				requiredElems.each(function () {
+					var type = $(this).attr("type");
 					type === "checkbox" || type === "radio"
-						? validateChRb(this)
-						: validate(this);
+						? validateCheckboxRadio(this)
+						: validateInput(this);
 				});
-
 				if (
-					$(
+					formData.find(
 						"." +
 							settings.errorFieldClass +
-							":visible,." +
+							":visible, ." +
 							settings.errorMsgClass +
 							":visible",
-						$(this),
-					).length <= 0
+					).length === 0
 				) {
 					if (typeof settings.beforeSubmit === "function") {
-						return settings.beforeSubmit.call(this, $form);
+						return settings.beforeSubmit.call(this, formData);
 					}
-					if ($formAjax) {
-						e.preventDefault();
+					if (ajax) {
 						$.ajax({
-							type: $form.attr("method"),
-							url: $form.attr("action"),
-							data: $form.serialize(),
+							type: form.attr("method"),
+							url: form.attr("action"),
+							data: formData.serialize(),
 							success: function (data) {
 								if (typeof callback === "function") {
-									callback.call(this, data, $form);
+									callback.call(this, data, formData);
 								}
 							},
 						});
+						return false;
+					} else {
+						return true;
 					}
-				} else {
-					e.preventDefault();
 				}
+				e.preventDefault();
 			});
 
-			// On Focus
-			valElems.on("focus", function (e) {
-				removeErrorMsg(e.target);
+			// On focus
+			form.find("input, select, textarea").on("focus", function () {
+				removeErrorMessage(this);
 			});
 
-			// On Blur
-			valElems.on("blur", function (e) {
-				validate(e.target);
+			// On blur
+			form.find("input, select, textarea").on("blur", function () {
+				validateInput(this);
 			});
 
-			// On click: checkbox & radio button
-			valChRaElems.on("click", function (e) {
-				validateChRb(e.target);
-			});
+			// On click for checkbox and radio button
+			form
+				.find("input[type='checkbox'], input[type='radio']")
+				.on("click", function () {
+					validateCheckboxRadio(this);
+				});
 		});
 	};
 })(jQuery);
